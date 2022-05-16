298dff5cbf020760bb1885e43301e9289e1db273
marionino.obog <marioo@magenic.com>
2022-05-16 19:17:56 +0800

    [mario] add test case & logic for createAccount

--- src/main/java/com/group3/exercise/bankapp/services/impl/InterestTransactionService.java
+++ src/main/java/com/group3/exercise/bankapp/services/impl/InterestTransactionService.java
@@ -1,13 +1,30 @@
 package com.group3.exercise.bankapp.services.impl;
 
 import com.group3.exercise.bankapp.entities.Account;
+import com.group3.exercise.bankapp.entities.InterestAccount;
 import com.group3.exercise.bankapp.services.TransactionService;
+import org.springframework.beans.factory.annotation.Value;
 
 public class InterestTransactionService implements TransactionService {
+
+    private Double interest;
+    private Double minimumBalance;
+
+    public InterestTransactionService(
+            @Value("${interest.charge:.03}") Double interest,
+            @Value("${interest.minimumBalance}") Double minimumBalance){
+        this.interest = interest;
+        this.minimumBalance = minimumBalance;
+    }
+
+
     @Override
     public Account generateNewAccountDetails(String name, String acctNumber, String type) {
         // TODO generate Account entity here.
-        return null;
+        Account account = new InterestAccount();
+        account.setInterestCharge(interest);
+        account.setMinimumBalance(minimumBalance);
+        return account;
     }
 
     @Override
--- src/test/java/com/group3/exercise/bankapp/services/impl/InterestTransactionServiceTest.java
+++ src/test/java/com/group3/exercise/bankapp/services/impl/InterestTransactionServiceTest.java
@@ -1,22 +1,40 @@
 package com.group3.exercise.bankapp.services.impl;
 
 
+import com.group3.exercise.bankapp.entities.Account;
+import com.group3.exercise.bankapp.entities.InterestAccount;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(MockitoExtension.class)
 public class InterestTransactionServiceTest {
 
-    @Mock
     InterestTransactionService interestTransactionService;
+
+    @BeforeEach
+    void setup(){
+    interestTransactionService = new InterestTransactionService(0.03, 0.0);
+    }
+
 
     @Test
     @DisplayName("Should generate correct details Interest Account")
     void shouldGenerateCorrectAccountDetails(){
-
+        // given
+        String name = "MARIO A";
+        String acctNbr = "12343555";
+        String type = "interest";
+        // when
+        Account actual = interestTransactionService.generateNewAccountDetails(name, acctNbr, type);
+        // then
+        assertEquals(actual.getInterestCharge(), 0.03);
+        assertEquals(actual.getMinimumBalance(), 0.0);
     }
     @Test
     @DisplayName("Should generate correct details Interest Account")
