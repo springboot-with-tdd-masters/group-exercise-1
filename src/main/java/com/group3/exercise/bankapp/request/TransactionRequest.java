cfff38d13ba2b2cd4833a3609d45f1f4e1d1e771
marionino.obog <marioo@magenic.com>
2022-05-30 18:46:37 +0800

    revert to last PR

--- src/main/java/com/group3/exercise/bankapp/request/TransactionRequest.java
+++ src/main/java/com/group3/exercise/bankapp/request/TransactionRequest.java
@@ -1,19 +1,8 @@
 package com.group3.exercise.bankapp.request;
 
-import com.group3.exercise.bankapp.constants.TransactionTypes;
-
-import java.util.Optional;
-
 public class TransactionRequest {
     private String type;
     private Double amount;
-
-    public TransactionRequest() {}
-
-    private TransactionRequest(String type, Double amount) {
-        this.type = type;
-        this.amount = amount;
-    }
 
     public String getType() {
         return type;
@@ -29,13 +18,5 @@ public class TransactionRequest {
 
     public void setAmount(Double amount) {
         this.amount = amount;
-    }
-
-    public Optional<TransactionTypes> getTypeAsTransactionTypesOptional() {
-        return TransactionTypes.findOptionalByLabel(this.type);
-    }
-
-    public static TransactionRequest of(String type, Double amount) {
-        return new TransactionRequest(type, amount);
     }
 }
